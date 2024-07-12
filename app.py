76bcfe92df76ee762b9d62dca7fdd072c087a214
felixxuss <felixm.mueller@hotmail.com>
2024-07-12 16:55:35 +0200

    added a disclaimer text

--- app.py
+++ app.py
@@ -1,23 +1,28 @@
 import streamlit as st
 
 from model import ModelWrapper
-from visuals import show_form, show_result
+from visuals import show_disclaimer, show_form, show_result
 
 st.set_page_config(page_title="CDSS")
-st.title("Heart Disease Prediction")
 
 if not st.session_state.get("model"):
     st.session_state["model"] = ModelWrapper()
 
-show_form()
+if "disclaimer_displayed" not in st.session_state:
+    st.session_state["disclaimer_displayed"] = False
 
-if st.session_state.get("result"):
-    # get model and result from session state
-    model = st.session_state["model"]
-    result = st.session_state["result"]
+if not st.session_state["disclaimer_displayed"]:
+    show_disclaimer()
+else:
+    st.title("Heart Disease Prediction")
+    show_form()
+    if st.session_state.get("result"):
+        # get model and result from session state
+        model = st.session_state["model"]
+        result = st.session_state["result"]
 
-    # Predicting the output
-    # has to return a number between 0 and 1
-    confidence, max_index = model.predict(result)
+        # Predicting the output
+        # has to return a number between 0 and 1
+        confidence, max_index = model.predict(result)
 
-    show_result(confidence, max_index)
+        show_result(confidence, max_index)
